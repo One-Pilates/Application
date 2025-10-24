1713639570dc95477d50c007f094bcfb7844eed0
Guilherme Queiroz de Lima <guilherme.lima@sptech.school>
2025-10-24 18:35:20 -0300

    Criando tela de Codigo Verificação

--- frontend/src/app/routes/PublicRoutes.jsx
+++ frontend/src/app/routes/PublicRoutes.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from '../features/Landing/Index';
 import Login from '../features/login/Login';
 import NotFound from './NotFound';
+import CodigoVerificacao from '../features/login/CodigoVerificacao';
 
 export default function PublicRoutes() {
     return (
         <Routes>
+            <Route path='/Codigo' element={<CodigoVerificacao/>}></Route>
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="*" element={<NotFound />} />
