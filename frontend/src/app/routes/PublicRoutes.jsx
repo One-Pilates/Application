3e08e4b5810750141e50fd0622eca86e87f68a30
Harison Rios de Miranda <hharison562@gmail.com>
2025-10-25 00:58:54 -0300

    fix: name Login

--- frontend/src/app/routes/PublicRoutes.jsx
+++ frontend/src/app/routes/PublicRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from '../features/Landing/Index';
-import Login from '../features/login/Login';
+import Login from '../features/Login/Login';
 import NotFound from './NotFound';
 import CodigoVerificacao from '../features/login/CodigoVerificacao';
 import EsqueciSenha from '../features/login/EsqueciSenha';
