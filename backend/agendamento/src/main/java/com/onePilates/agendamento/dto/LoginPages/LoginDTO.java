e12a0221cdf16e50cf9c40fa3177caba63111e30
Guilherme Queiroz de Lima <guilherme.lima@sptech.school>
2025-11-12 19:27:38 -0300

    Colocando logica para alteração de senha com envio das credenciais por email

--- backend/agendamento/src/main/java/com/onePilates/agendamento/dto/LoginPages/LoginDTO.java
+++ backend/agendamento/src/main/java/com/onePilates/agendamento/dto/LoginPages/LoginDTO.java
@@ -1,4 +1,4 @@
-package com.onePilates.agendamento.dto;
+package com.onePilates.agendamento.dto.LoginPages;
 
 public class LoginDTO {
     private String email;
--- backend/agendamento/src/main/java/com/onePilates/agendamento/service/AuthService.java
+++ backend/agendamento/src/main/java/com/onePilates/agendamento/service/AuthService.java
@@ -1,10 +1,10 @@
 package com.onePilates.agendamento.service;
 
-import com.onePilates.agendamento.dto.EspecialidadeDTO;
 import com.onePilates.agendamento.dto.FuncionarioLoginDTO;
-import com.onePilates.agendamento.dto.LoginDTO;
+import com.onePilates.agendamento.dto.LoginPages.LoginDTO;
 import com.onePilates.agendamento.dto.response.EspecialidadeResponseDTO;
 import com.onePilates.agendamento.dto.response.LoginResponseDTO;
+import com.onePilates.agendamento.dto.response.NovaSenhaResponseDTO;
 import com.onePilates.agendamento.model.Funcionario;
 import com.onePilates.agendamento.model.Professor;
 import com.onePilates.agendamento.model.Role;
@@ -16,7 +16,10 @@ import com.onePilates.agendamento.security.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,20 +29,24 @@ public class AuthService {
     private final ProfessorRepository professorRepository;
     private final SecretariaRepository secretariaRepository;
     private final PasswordEncoder passwordEncoder;
+    private final FuncionarioRepository funcionarioRepository;
     private final JwtUtil jwtUtil;
+    private final EmailService emailService;
 
     public AuthService(
             AdministradorRepository administradorRepository,
             ProfessorRepository professorRepository,
             SecretariaRepository secretariaRepository,
-            PasswordEncoder passwordEncoder,
-            JwtUtil jwtUtil
+            PasswordEncoder passwordEncoder, FuncionarioRepository funcionarioRepository,
+            JwtUtil jwtUtil, EmailService emailService
     ) {
         this.administradorRepository = administradorRepository;
         this.professorRepository = professorRepository;
         this.secretariaRepository = secretariaRepository;
         this.passwordEncoder = passwordEncoder;
+        this.funcionarioRepository = funcionarioRepository;
         this.jwtUtil = jwtUtil;
+        this.emailService = emailService;
     }
 
     public LoginResponseDTO authenticate(LoginDTO request) {
@@ -111,5 +118,57 @@ public class AuthService {
                 .or(() -> professorRepository.findByEmail(email).map(f -> (Funcionario) f))
                 .or(() -> secretariaRepository.findByEmail(email).map(f -> (Funcionario) f))
                 .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    public String criarCodigoVerificacao(String email) {
+        Funcionario funcionario = buscarFuncionarioPorEmail(email);
+
+        String codigoVerificacao = String.format("%05d", new Random().nextInt(100000));
+        funcionario.setCodigoVerificacao(codigoVerificacao);
+        funcionario.setDataUltimaCriacaoCodigo(LocalDateTime.now());
+
+        funcionarioRepository.save(funcionario);
+
+        try {
+            emailService.enviarCodigoPorEmail(funcionario.getNome(), codigoVerificacao, email);
+        } catch (Exception e) {
+            System.err.println("Erro ao enviar o e-mail de verificação: " + e.getMessage());
+            return "Erro ao enviar o e-mail de verificação.";
+        }
+
+        return "Código enviado para o e-mail do funcionário.";
+    }
+
+
+    public Boolean validarCodigoVerificacao(String email, String codigoVerificacao) {
+        Funcionario funcionario = buscarFuncionarioPorEmail(email);
+
+
+
+        if (!funcionario.getCodigoVerificacao().equals(codigoVerificacao)) {
+            throw  new RuntimeException("Código inválido");
+        }
+
+
+        LocalDateTime agora = LocalDateTime.now();
+        Duration diferenca = Duration.between(funcionario.getDataUltimaCriacaoCodigo(), agora);
+
+        if (diferenca.toMinutes() > 5) {
+            throw  new RuntimeException ("Código expirado");
+        }
+
+        return true;
+    }
+
+    public NovaSenhaResponseDTO novaSenha(String senha,String email) {
+        Funcionario funcionario = buscarFuncionarioPorEmail(email);
+        if(funcionario==null){
+            throw new RuntimeException("Funcionário não encontrado");
+        }
+        funcionario.setSenha(passwordEncoder.encode(senha));
+        funcionarioRepository.save(funcionario);
+        NovaSenhaResponseDTO dtoResponse =  new NovaSenhaResponseDTO();
+        dtoResponse.setMensagem("Senha alterada com sucesso");
+        return dtoResponse;
     }
 }
